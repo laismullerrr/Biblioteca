7a136255ef717b0b9428f272c4a6e53f33511b6c
laismullerrr <lais.muller@hotmail.com>
2023-07-31 22:17:04 -0300

    inserts tabela "editora"

---
+++ inserts_editora.sql
@@ -0,0 +1,11 @@
+USE BIBLIOTECA;
+
+INSERT INTO Editora (id, nome, endereco)
+VALUES
+  (1, 'Editora Rocco', 'Rua Marquês de São Vicente, 99 - Rio de Janeiro'),
+  (2, 'Editora Leya', 'Rua Luís Antônio de Souza, 347 - São Paulo'),
+  (3, 'Intrínseca', 'Rua Marquês de São Vicente, 99 - Rio de Janeiro'),
+  (4, 'Hyperion Kids', '1234 Story Avenue, New York'),
+  (5, 'Scholastic Paperbacks', '123 Main Street, New York'),
+  (6, 'HarperCollins', '123 Book Avenue, New York'),
+  (7, 'Suma', 'Rua dos Livros, 123 - São Paulo');
