659cf569bd211d3867337c2f468fa60ac3f8dc79
laismullerrr <lais.muller@hotmail.com>
2023-07-31 22:13:46 -0300

    inserts tabela "livro"

---
+++ inserts_livro.sql
@@ -0,0 +1,29 @@
+USE BIBLIOTECA;
+
+INSERT INTO Livro (id, titulo, ano_publicacao, autor_id, editora_id, idioma_id)
+VALUES
+  (1, 'Harry Potter e a Pedra Filosofal', 1997, 1, 1, 1),
+  (2, 'Harry Potter e a Câmara Secreta', 1998, 1, 1, 1),
+  (3, 'Harry Potter e o Prisioneiro de Azkaban', 1999, 1, 1, 1),
+  (4, 'Harry Potter e o Cálice de Fogo', 2000, 1, 1, 1),
+  (5, 'Harry Potter e a Ordem da Fênix', 2003, 1, 1, 1),
+  (6, 'Harry Potter e o Enigma do Príncipe', 2005, 1, 1, 1),
+  (7, 'Harry Potter e as Relíquias da Morte', 2007, 1, 1, 1),
+  (8, 'Percy Jackson e o Ladrão de Raios', 2005, 3, 3, 1),
+  (9, 'Percy Jackson e o Mar de Monstros', 2006, 3, 3, 1),
+  (10, 'Percy Jackson e a Maldição do Titã', 2007, 3, 3, 1),
+  (11, 'Percy Jackson e a Batalha do Labirinto', 2008, 3, 3, 1),
+  (12, 'Percy Jackson e o Último Olimpiano', 2009, 3, 3, 1),
+  (13, 'Percy Jackson and The Lightning Thief', 2005, 3, 4, 2),
+  (14, 'Harry Potter and the Sorcerers Stone', 1997, 1, 5, 2),
+  (15, 'As Crônicas de Nárnia: O Leão, a Feiticeira e o Guarda-Roupa', 1950, 4, 6, 1),
+  (16, 'As Crônicas de Nárnia: Príncipe Caspian', 1951, 4, 6, 1),
+  (17, 'As Crônicas de Nárnia: A Viagem do Peregrino da Alvorada', 1952, 4, 6, 1),
+  (18, 'As Crônicas de Nárnia: A Cadeira de Prata', 1953, 4, 6, 1),
+  (19, 'As Crônicas de Nárnia: A Última Batalha', 1956, 4, 6, 1),
+  (20, 'Caixa de Pássaros', 2014, 5, 3, 1),
+  (21, 'Inspection', 2019, 5, 3, 2),
+  (22, 'Piano Vermelho', 2017, 5, 3, 1),
+  (23, 'It: A Coisa', 1986, 6, 7, 1),
+  (24, 'The Shining', 1977, 6, 7, 2),
+  (25, 'Carrie, a Estranha', 1974, 6, 7, 1);
