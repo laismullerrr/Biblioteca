3e0d2b06fe225fded96f4b6124ea01e3632c2bed
laismullerrr <lais.muller@hotmail.com>
2023-07-31 22:20:26 -0300

    inserts tabela "usuário"

---
+++ inserts_usuario.sql
@@ -0,0 +1,14 @@
+USE BIBLIOTECA;
+
+INSERT INTO Usuario (id, nome, endereco, email, telefone)
+VALUES
+  (1, 'João Silva', 'Rua das Flores, 123 - São Paulo', 'joao.silva@gmail.com', '(11) 1234-5678'),
+  (2, 'Maria Santos', 'Avenida das Palmeiras, 456 - Rio de Janeiro', 'maria.santos@hotmail.com', '(21) 9876-5432'),
+  (3, 'Pedro Souza', 'Rua das Pedras, 789 - Belo Horizonte', 'pedro.souza@outlook.com', '(31) 6543-2109'),
+  (4, 'Ana Lima', 'Travessa das Oliveiras, 987 - Porto Alegre', 'ana.lima@gmail.com', '(51) 8765-4321'),
+  (5, 'Lucas Oliveira', 'Alameda dos Ipês, 543 - Brasília', 'lucas.oliveira@hotmail.com', '(61) 1234-5678'),
+  (6, 'Mariana Costa', 'Rua das Acácias, 789 - Salvador', 'mariana.costa@gmail.com', '(71) 9876-5432'),
+  (7, 'Rafael Oliveira', 'Avenida dos Pinheiros, 321 - São Paulo', 'rafael.oliveira@hotmail.com', '(11) 8765-4321'),
+  (8, 'Carolina Martins', 'Travessa das Rosas, 543 - Curitiba', 'carolina.martins@hotmail.com', '(41) 1234-5678'),
+  (9, 'Fernando Santos', 'Alameda dos Coqueiros, 765 - Recife', 'fernando.santos@gmail.com', '(81) 9876-5432'),
+  (10, 'Camila Lima', 'Rua das Laranjeiras, 456 - Fortaleza', 'camila.lima@outlook.com', '(85) 8765-4321');
